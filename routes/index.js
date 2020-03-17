fb99503b558957c790509e3b6a36848f16d3b116
farukh.sultan <farukh.5937@gmail.com>
2020-03-17 23:29:29 +0500

    Landing Page code

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var user_Route = require('./users');
 var store_Route = require('./store');
+//var search_Route = require('./search');
+var search_Route = require('./search');
 const mongoose = require('mongoose');
 
 /* GET home page. */
@@ -11,7 +13,8 @@ router.get('/', function(req, res, next) {
 //Basic User 
 router.use('/user', user_Route);
 router.use('/store', store_Route);
-// 
+// Search Store
+router.use('/search', search_Route);
 
 mongoose.connect(
   'mongodb+srv://Farukh:110581F.A@rest-6ss50.mongodb.net/test?retryWrites=true&w=majority',
--- routes/search.js
+++ routes/search.js
@@ -1,3 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var store_services = require('../services/store');
+var search_services = require('../services/search');
+
+//Get Search Result
+
+router.post('/', search_services.get_store);
+module.exports = router;
